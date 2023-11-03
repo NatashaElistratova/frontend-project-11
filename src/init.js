7f47ceac5ce8750430a19fb1d64dfd69db1972a5
natalia.yelistratova <natalia.yelistratova@gmail.com>
2023-11-03 22:12:06 +0300

    Add i18next

--- src/init.js
+++ src/init.js
@@ -1,11 +1,28 @@
 import * as yup from 'yup';
+import i18next from 'i18next';
 import onChange from 'on-change';
 import isEmpty from 'lodash/isEmpty.js';
+import resources from './locales/index.js';
 import watch from './view.js';
 
-export default () => {
+export default async () => {
   const form = document.querySelector('[data-form="rss-form"]');
   const urlInput = document.querySelector('#url-input');
+  const defaultLang = 'ru';
+  const i18n = i18next.createInstance();
+  await i18n.init({
+    lng: defaultLang,
+    debug: false,
+    resources,
+  });
+
+  yup.setLocale({
+    string: {
+      required: () => ({ key: 'errors.validation.required' }),
+      url: () => ({ key: 'errors.validation.url' }),
+      notOneOf: () => ({ key: 'errors.validation.notOneOf' }),
+    },
+  });
 
   const initialState = {
     feeds: [],
@@ -18,17 +35,18 @@ export default () => {
     },
   };
 
-  const state = onChange(initialState, watch(urlInput, initialState));
+  const state = onChange(initialState, watch(urlInput, initialState, i18n));
 
-  const schema = yup.string().url('Ссылка должна быть валидным URL');
+  const schema = yup.string().url();
 
   const validateUrl = async (el, feeds) => {
-    const actualSchema = schema.notOneOf(feeds, 'RSS уже существует');
+    const actualSchema = schema.notOneOf(feeds);
     try {
       await actualSchema.validate(el, { abortEarly: false });
       return {};
     } catch (e) {
-      return { urlInput: e.message };
+      const [message] = e.errors.map((err) => i18n.t(err.key));
+      return { urlInput: message };
     }
   };
 
---
+++ src/locales/ru.js
@@ -0,0 +1,14 @@
+export default {
+  translation: {
+    languages: {
+      ru: 'Русский',
+    },
+    errors: {
+      validation: {
+        required: 'Это обязательное поле',
+        url: 'Ссылка должна быть валидным URL',
+        notOneOf: 'RSS уже существует',
+      },
+    },
+  },
+};
